a2929ebc46eb2b90d65ca0538c4911de886eb370
London <10482713+london-c@users.noreply.github.com>
2018-10-18 15:38:56 -0700

    New logic to run basic commands (alpha)

--- turl/logic/args.swift
+++ turl/logic/args.swift
@@ -13,8 +13,39 @@ import Foundation
 //Array of the kind of arguments that can be used in Turl currently
 var args = ["help", "link"]
 
-func checkArgument(withValue value: String) {
-    if args.contains(value) {
-        print("contains value:",value)
+enum argType: CaseIterable {
+    case help
+    case link
+}
+
+func checkArgument(withValue value: [String]) {
+    for values in value {
+        if args.contains(values) {
+            
+            //TODO: Update this so it can detect instead of using if statements
+            //call something here to run based on what argument was called
+            if(values.contains("help")) {
+                argFunction(usingArg: .help)
+            } else if(values.contains("link")) {
+                argFunction(usingArg: .link)
+            }
+        }
+    }
+}
+
+func argFunction(usingArg arg: argType) {
+    
+    switch arg {
+        
+    case .help:
+        print("This is the help command!")
+        
+    case .link:
+        if cl_args.count > 2 && cl_args.last!.contains(".com") {
+            let link = cl_args.last
+            print("Success: Link was included", link!)
+        } else {
+            print("Error 1: Link is required")
+        }
     }
 }
--- turl/main.swift
+++ turl/main.swift
@@ -9,14 +9,14 @@
 import Foundation
 import CoreServices
 
+//MARK: Properties
 var p = Process()
 var cl_args = CommandLine.arguments
 
+//FIXME: Update this to detect if there was multiple commands called
 if(cl_args.count < 2) {
     print("Error 0: No arguments defined")
     exit(0)
 } else {
-    checkArgument(withValue: cl_args.last!)
+    checkArgument(withValue: cl_args)
 }
-
-print("turl: There were \(cl_args.count) successful inputs!")
